f89b25525a96165298db9d3aff2a4cc511a9c2db
asifhussainkaksar <110120012+asifhussainkaksar@users.noreply.github.com>
2022-07-27 22:29:50 +0530

    Update calculator.c

--- calculator.c
+++ calculator.c
@@ -1,20 +1,20 @@
 #include<stdio.h>
 int main(){
-  int a, b;
+  float a, b;
   int n;
   printf("Select your choice : \n1. Addition \n2. Subtraction \n3.Multiplication \n4. Division");
   scanf("%d", &n);
   printf("Enter two numbers : ");
-  scanf("%d %d", &a, &b);
+  scanf("%f %f", &a, &b);
   switch(n){
     case 1:
-        printf("Addition is %d ", a+b);
+        printf("Addition is %f ", a+b);
         break;
     case 2:
-         printf("Difference is %d ", a-b);
+         printf("Difference is %f ", a-b);
          break;
     case 3:
-          printf("Multiplication is %d ", a*b);
+          printf("Multiplication is %f ", a*b);
          break;
     case 4:
           printf("Division is %f ", a/b);

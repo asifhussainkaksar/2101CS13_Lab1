211867927e9b0cb81eea220032a7d2f1601e7d8c
asifhussainkaksar <110120012+asifhussainkaksar@users.noreply.github.com>
2022-07-27 22:39:25 +0530

    selection sort

--- Sorting.c
+++ Sorting.c
@@ -1,5 +1,19 @@
 #include <stdio.h>
 
+void selection_sort(int array[], int n){
+  for(int i = 0;i<n-1;i++){
+    for(int j = i+1;j<n;j++){
+      if(array[i]>array[j]){
+        int temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
+      }
+    }
+  }
+  for(int i = 0;i<n;i++){
+      printf("%d  ", array[i]);
+    }
+}
 int main(){
   int n;
   printf("Enter the value of n");

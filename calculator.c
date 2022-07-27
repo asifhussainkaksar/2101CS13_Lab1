aff1c43d1ee1153df10e394ffe53c0913eaf6ce8
asifhussainkaksar <110120012+asifhussainkaksar@users.noreply.github.com>
2022-07-27 22:23:28 +0530

    Addition

---
+++ calculator.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main(){
+  int a, b;
+  int n;
+  printf("Select your choice : \n1. Addition \n2. Subtraction \n3.Multiplication \n4. Division");
+  scanf("%d", &n);
+  printf("Enter two numbers : ");
+  scanf("%d %d", &a, &b);
+  switch(n){
+    case 1:
+        printf("Addition is %d ", a+b);
+        break;
+        
